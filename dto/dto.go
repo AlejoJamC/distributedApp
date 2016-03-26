ee7c965ce6a7c5ac498d1b03e6c3a12c9f975f81
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2016-03-25 23:27:35 -0500

    Data Transfer Object package

---
+++ dto/dto.go
@@ -0,0 +1,17 @@
+// Data Transfer Object
+package dto
+
+import (
+	"encoding/gob"
+	"time"
+)
+
+type SensorMessage struct {
+	Name string
+	Value float64
+	Timestamp time.Time
+}
+
+func init() {
+	gob.Register(SensorMessage{})
+}
